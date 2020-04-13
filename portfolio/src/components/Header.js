8224c28e7a6154b4d6005319d70c1fc4474e7945
Dev-miniyoung <miniyoung37@gmail.com>
2020-04-13 14:47:52 +0900

    add Header component

---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <span>소개</span>
+      <span>PortFolio</span>
+      <span>Contact</span>
+    </div>
+  );
+};
+
+export default Header;
